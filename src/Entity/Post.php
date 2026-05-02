39e630764f00369f1d6ce7cd45cce3a178aea928
debz-cpu <brizwid.riri@gmail.com>
2026-05-02 12:18:05 +0200

    feat: fix for establishing user-post relationship with OneToMany and ManyToOne associations

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -29,6 +29,10 @@ class Post
     #[ORM\Column]
     private ?\DateTime $updatedAt = null;
 
+    #[ORM\ManyToOne(inversedBy: 'posts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -90,6 +94,18 @@ class Post
     public function setUpdatedAt(\DateTime $updatedAt): static
     {
         $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
 
         return $this;
     }
